b1981ef610ed3f3e050911de95236dbea1527454
AbhishekSinghkirola <studyabhi82662@gmail.com>
2023-05-27 19:40:55 +0530

    Docker Setup

--- src/public/index.php
+++ src/public/index.php
@@ -2,17 +2,5 @@
 
 declare(strict_types=1);
 
-// phpinfo();
+phpinfo();
 
-/** Classes And Objects */
-require "../Transaction.php";
-
-$transaction = new Transaction(100, "Description1");
-
-$transaction->addTax(8)->applyDiscount(10);
-$amount = $transaction->getAmount();
-
-// unset($transaction);
-var_dump($amount);
-
-var_dump((object) "Abhishek");
